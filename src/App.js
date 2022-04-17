2c05777d268272ad2b14b055928b959f6ab5176e
Moin Khan <nurmoin05@gmail.com>
2022-04-18 00:33:56 +0600

    user login added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+import './Login.css';
+import useFirebase from './../../hooks/useFirebase';
+
+const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const { loginUser, user } = useFirebase();
+
+    // Handle Form 
+    const handleForm = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        loginUser(email, password);
+
+    }
+
+    return (
+        <div className="Login">
+            <div className="inner__login container">
+                <div className="login__form">
+                    <form onSubmit={handleForm}>
+                        <div className="form__title">
+                            <h2>Login</h2>
+                        </div>
+                        <div className="form__group">
+                            <div className="input__group">
+                                <label htmlFor="email">Email</label>
+                                <input ref={emailRef} type="email" name="email" id="email" />
+                            </div>
+                            <div className="input__group">
+                                <label htmlFor="password">Password</label>
+                                <input ref={passwordRef} type="password" name="password" id="password" />
+                            </div>
+                            <div className="form__submit__btn">
+                                <input type="submit" value="Login" />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
