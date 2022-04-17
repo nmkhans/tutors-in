a96a6c90824bca71180990c02caacc3947f408ba
Moin Khan <nurmoin05@gmail.com>
2022-04-17 17:27:55 +0600

    hero area completed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
+        <div className="Home">
+            <div className="inner__home container">
+                <div className="home__content">
+                    <div className="hero__area">
+                        <div className="hero__left__text">
+                            <h3>Your Personalized Learning Program</h3>
+                            <h2>Learn effectively and in a innovative way that you never had before.</h2>
+                            <button>Choose a Plan</button>
+                        </div>
+                        <div className="hero__right__img">
+                            <img src="https://i.ibb.co/L8ggwWM/teacher.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
