e05252a358e2b137b98fde87763301756b26851f
Moin Khan <nurmoin05@gmail.com>
2022-04-17 15:13:49 +0600

    project reinitialized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      
     </div>
   );
 }
