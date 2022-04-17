296c41ba31475c8d37e0307622f4ca4684ddb219
Moin Khan <nurmoin05@gmail.com>
2022-04-17 21:35:38 +0600

    besic design done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -18,7 +18,6 @@ const Home = () => {
                                     <img src="https://i.ibb.co/L8ggwWM/teacher.png" alt="" />
                                 </div>
                             </div>
-
                         </div>
                     </div>
                     <div className="package__area">
@@ -65,6 +64,21 @@ const Home = () => {
                                     <div className="package__button">
                                         <button>Read More</button>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="intro__area">
+                        <div className="inner__intro__area container">
+                            <div className="intro__content">
+                                <div className="intro__left__img">
+                                    <img src="https://i.ibb.co/nDBc85s/Internship-pana.png" alt="" />
+                                </div>
+                                <div className="intro__right__desc">
+                                    <h2>Hi I'm Alex Costa!</h2>
+                                    <h3>Personal and Professional Tutor</h3>
+                                    <p>I am a professional and passionated Home Tutor who can make your study easier and enjoyable in an effective and innovative way. I am here to help you out with your study and make you improve your self day by day without even any boaringness. You can let me take care of your study and make you improve. I can teach you personally and in a customizable study program according to your requirement. You can have me to guide you to the right path way toward your success. So without any further grab one package and start with me today.</p>
+                                    <button>Choose a Plan</button>
                                 </div>
                             </div>
                         </div>
